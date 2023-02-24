375417752977d6d2501b83f4370f29a319b3e511
Jerry4753 <74114017+Jerry4753@users.noreply.github.com>
2023-02-24 11:22:43 -0500

    First Change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello There!</p>
+        <p>Hello There 2!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
